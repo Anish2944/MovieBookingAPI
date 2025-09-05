090a7bcccb79f613409efe8b968a6a5c4f9d3ec9
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2025-09-05 23:44:45 +0530

    hh

--- .Net/Movie_Tickets/Program.cs
+++ .Net/Movie_Tickets/Program.cs
@@ -6,6 +6,10 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
 // Add EF Core with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
