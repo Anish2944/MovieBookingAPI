bb892bb6511273e75933f41ab839363df2cfa150
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2025-09-20 23:27:04 +0530

    image url added

--- .Net/Movie_Tickets/Program.cs
+++ .Net/Movie_Tickets/Program.cs
@@ -42,7 +42,10 @@ builder.Services.AddAuthentication("Bearer")
 
 // Add EF Core with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+      options.UseSqlServer(
+          builder.Configuration.GetConnectionString("DefaultConnection"),
+          sqlOptions => sqlOptions.EnableRetryOnFailure()
+      ));
 
 builder.Services.AddScoped<JwtService>();
 
